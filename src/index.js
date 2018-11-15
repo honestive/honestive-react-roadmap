833cef45436ee61fbd46210eada588421b9ec90a
Domas <domas.bitvinskas@me.com>
2018-11-15 18:56:48 +0000

    More flexible styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider, Query } from 'react-apollo'
 import gql from 'graphql-tag'
@@ -7,6 +7,7 @@ import {
   Container,
   Button,
   CandourProvider,
+  Link,
 } from 'candour'
 import candourTheme from './theme/candour'
 
@@ -15,83 +16,80 @@ const query = gql`
     releases {
       id
       version
+      url
     }
   }
 `
 
 const isLast = (releases, id) => _.last(releases).id === id
 
+const DefaultPoint = (props) => (
+  <Button
+    component='a'
+    target='_blank'
+    borderRadius='9999px'
+    whiteSpace='nowrap'
+    {...props}
+  />
+)
+
+const DefaultLine = (props) => (
+  <Container width={1.5} height='1px' backgroundColor='#000' />
+)
+
 export default ({
   last = 5,
   endpoint = 'http://localhost:4000',
+  components,
+  ...props,
 }) => (
-  <CandourProvider theme={candourTheme}>
+  <CandourProvider {...props}>
     <ApolloProvider client={new ApolloClient({ uri: endpoint })}>
       <Query query={query}>
         {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>
           if (error) return <p>Error :(</p>
 
           const {
             releases,
-          } = data
+          } = data || { releases: _.map(_.times(last), (id) => { id }) }
+
+          const {
+            Point,
+            Line,
+          } = _.defaultsDeep(components, { Point: DefaultPoint, Line: DefaultLine })
 
           return (
-            <Container displayFlex paddingBottom>
-              {_.takeRight(releases, last).map(({ id, version }, i) => (
-                <Container displayFlex alignItemsCenter width={isLast(releases, id) ? '100%' : 'initial'}>
-                  <Container
-                    key={id}
-                    style={styles.dot.container}
-                    padding
-                    paddingTop={0.5}
-                    paddingBottom={0.5}
-                  >
-                    <Text fontWeightBold whiteSpace='nowrap'>
-                      {version}
-                    </Text>
+            <Fragment>
+              <Container displayFlex paddingBottom width='100%'>
+                {_.takeRight(releases, last).map((release, i) => (
+                  <Container key={release.id} displayFlex alignItemsCenter>
+                    <Point href={release.url} {...release}>
+                      {release.version}
+                    </Point>
+                    <Line />
                   </Container>
-                  <Container width={isLast(releases, id) ? '100%' : 1.5} height='2px' backgroundColor='#000' />
+                ))}
+                <Container displayFlex alignItemsCenter width='100%'>
+                  <Point href='/future'>
+                    ···
+                  </Point>
+                  <Line width='100%' />
                 </Container>
-              ))}
-            </Container>
+              </Container>
+              <Text marginLeftAuto textAlignRight level={8}>
+                <Link
+                  component='a'
+                  href='https://honestive.com'
+                  target='_blank'
+                  level={8}
+                >
+                  ■ Honestive
+                </Link>
+              </Text>
+            </Fragment>
           )
         }}
       </Query>
     </ApolloProvider>
-    <Container displayFlex alignItems='flex-end' justifyContentSpaceBetween paddingTop={2}>
-      <Button
-        component='a'
-        href='http://github.com'
-        backgroundColor='transparent'
-        color='#2765EF'
-        border='2px solid #2765EF'
-        width='inherit'
-        height='40px'
-        fontWeight={450}
-      >
-        View on GitHub
-      </Button>
-      <Text textAlignRight paddingTop>
-        ⚡️ by <Text
-          component='a'
-          href='https://honestive.com'
-          target='_blank'
-          style={styles.link}
-          color='#2765EF'
-        >
-          Honestive
-        </Text>
-      </Text>
-    </Container>
   </CandourProvider>
 )
-
-const styles = {
-  dot: {
-    container: {
-      border: '2px solid #000',
-      borderRadius: '9999px',
-    },
-  },
-}
